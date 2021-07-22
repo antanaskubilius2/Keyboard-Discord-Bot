8f906089373d902b9bf3158e2aa6ea7902cf6f94
Antanas Kubilius <70003998+antanaskubilius2@users.noreply.github.com>
2021-07-21 20:12:02 -0500

    Added Commands
    Updated commands added enought to power and takeup 2 gigabytes of ram

--- discord bot/Discord/src/commands/test/TestCommand.js
+++ discord bot/Discord/src/commands/test/TestCommand.js
@@ -6,7 +6,7 @@ module.exports = class TestCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-    message.channel.send('Online, Check update logs on my Github');
+    message.channel.send('Online, Check update logs on my Github Link-> https://github.com/antanaskubilius2/Keyboard-Discord-Bot');
   }
 }
 
@@ -17,5 +17,76 @@ module.exports = class TestCommand extends BaseCommand {
 
   async run(client, message, args) {
     message.channel.send('My youtube is Antanas Keyboards but heres a link https://www.youtube.com/channel/UCCvb6nxzb5vFmnJZg1OpIFQ make sure to subscribe');
+  }
+}
+
+module.exports = class TestCommand extends BaseCommand {
+  constructor() {
+    super('info', 'testing', []);
+  }
+
+  async run(client, message, args) {
+    message.channel.send('This bot was made in TypeScript and JavaScript ');
+  }
+}
+
+module.exports = class TestCommand extends BaseCommand {
+  constructor() {
+    super('creators', 'testing', []);
+  }
+
+  async run(client, message, args) {
+    message.channel.send('Antanas K');
+  }
+}
+
+module.exports = class TestCommand extends BaseCommand {
+  constructor() {
+    super('botinfo', 'testing', []);
+  }
+
+  async run(client, message, args) {
+    message.channel.send('Used VS Code. Also Development took 100Hours+ Thanks for supporting');
+  }
+}
+
+
+module.exports = class TestCommand extends BaseCommand {
+  constructor() {
+    super('boot', 'testing', []);
+  }
+
+  async run(client, message, args) {
+    message.channel.send('Booting VIA RBS ...');
+  }
+}
+
+module.exports = class TestCommand extends BaseCommand {
+  constructor() {
+    super('runtime', 'testing', []);
+  }
+
+  async run(client, message, args) {
+    message.channel.send('running for long enough, thanks to Antanas for keeping me up!');
+  }
+}
+
+module.exports = class TestCommand extends BaseCommand {
+  constructor() {
+    super('node', 'testing', []);
+  }
+
+  async run(client, message, args) {
+    message.channel.send('bootstrap with npm');
+  }
+}
+
+module.exports = class TestCommand extends BaseCommand {
+  constructor() {
+    super('run', 'testing', []);
+  }
+
+  async run(client, message, args) {
+    message.channel.send('running :tools:');
   }
 }
